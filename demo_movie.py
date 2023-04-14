a53fb29a5b585793f10e700d7b6ef28402923340
Jonathan Palafoutas <jpalafou@princeton.edu>
2023-04-14 07:41:27 -0400

    initial commit

---
+++ demo_movie.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import os
+from PIL import Image
+from time import time
+from spring import SpringCoupledMasses
+
+# define system and solve
+system = SpringCoupledMasses(
+    m = [1,1,1],
+    pairs = [(0,1), (0,2), (1,2)],
+    stiffness = [1, 1, 1],
+    damping = [0.5, 0.5, 0.5]
+)
+system.set_initial_condition(
+    x0 = np.array([[np.sqrt(3) / 2, -1 / 2], [0, 1/ 2], [-np.sqrt(3) / 2, -1 / 2]]),
+    v0 = np.array([[0, 0.1], [0, 0], [0, 0]]),
+    )
+start = time()
+system.rk4(T = 5, timesteps = 50001, frames = 101)
+print(f"Solved in {time() - start:.2f} s")
+
+
+# make video
+folder_path = 'frames'
+for frame in range(101):
+    for p in range(len(system.m)):
+        plt.plot(system.X[frame, p, 0], system.X[frame, p, 1], 'o', label = f"m{p}")
+    for pi, pj in system.pairs:
+        plt.plot(system.X[frame, (pi, pj), 0], system.X[frame, (pi, pj), 1], '--', color='grey', label = f"m{p}")
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.xlim([-1, 1])
+    plt.ylim([-1, 1])
+    plt.savefig(f"{folder_path}/{frame:03d}.png")
+    plt.close()
+print(f"Saved frames to {folder_path}")
+
+# Sort the PNG images by filename
+png_files = sorted([f for f in os.listdir(folder_path) if f.endswith('.png')])
+
+# Create a list to hold the images
+images = []
+for png_file in png_files:
+    # Open each PNG image and append it to the list
+    image = Image.open(os.path.join(folder_path, png_file))
+    images.append(image)
+
+# Save the list of images as a GIF
+images[0].save('demo.gif', save_all=True, append_images=images[1:], duration=0, loop=0)
+print(f"Generated gif")
---
+++ spring.py
@@ -0,0 +1,106 @@
+from numpy.random import rand
+import numpy as np
+
+class SpringCoupledMasses():
+    def __init__(self, m, pairs, stiffness = None, damping = None):
+        """
+        solver for system of point masses connected by damped springs
+
+        args:
+            m           list of masses
+            pairs       list of tuples of mass indices
+            stiffness   list of stiffness values
+            damping     list of damping values
+        returns:
+            K           stiffness matrix
+            B           damping matrix
+            A           linear operator
+        """
+        n = len(m)
+        self.n = n
+        self.pairs = pairs
+        self.m = m
+        self._get_stiffness_and_damping(pairs, stiffness, damping)
+        I = np.identity(n)
+        ones = np.ones(n)
+        # mass matrix
+        minv = np.diag(np.reciprocal(np.array(m), dtype='float64'))
+        # stiffness and damping operators
+        A_stiffness = minv @ (self.K - np.diag(self.K @ ones))
+        A_damping = minv @ (self.B - np.diag(self.B @ ones))
+        # combined linear operator
+        A = np.zeros((2 * n, 2 * n))
+        A[:n, n:] = I
+        A[n:, :n] = A_stiffness
+        A[n:, n:] = A_damping
+        self.A = A
+        # eigenvalues and stiffness
+        self.eigvals = np.linalg.eig(A)[0]
+        self.numerical_stiffness = np.max(np.abs(self.eigvals)) / np.min(np.abs(self.eigvals))
+
+    def _get_stiffness_and_damping(self, pairs, stiffness, damping):
+        K = np.zeros((self.n, self.n)) # stiffness matrix
+        B = np.zeros((self.n, self.n)) # damping matrix
+        if stiffness is not None:
+            for i, k in zip(pairs, stiffness):
+                K[i] = k
+                K[i[::-1]] = k
+        if damping is not None:
+            for i, b in zip(pairs, damping):
+                B[i] = b
+                B[i[::-1]] = b
+        self.K = K
+        self.B = B
+
+    def set_initial_condition(self, x0 = None, v0 = None, d = 2):
+        """
+        args:
+            x0  array of shape (n,d) where d is dimensionality
+            v0  like x0
+        """
+        if x0 is None:
+            x0 = 2 * rand(self.n, d) - 1
+        if v0 is None:
+            v0 = 2 * rand(self.n, d) - 1
+        self.x0 = x0 # d-dimensional position
+        self.v0 = v0 # d-dimensional velocity
+
+    def rk4(self, T = 1., timesteps = 101, frames = 11):
+        """
+        args:
+            T           integration stopping time
+            timesteps   number of discretized time values
+            frames      number of time values to save at
+        """
+        # intialize storage
+        self.t = np.zeros(frames)
+        U0 = np.concatenate((self.x0, self.v0))
+        U1 = np.concatenate((np.zeros(self.x0.shape),np.zeros(self.v0.shape)))
+        self.X = np.repeat(np.zeros(self.x0.shape)[None,...],frames,axis=0)
+        self.V = np.repeat(np.zeros(self.v0.shape)[None,...],frames,axis=0)
+        self.X[0] = self.x0
+        self.V[0] = self.v0
+        # indices to save at
+        idx_log = np.linspace(0, timesteps - 1, frames).astype(int)
+        # time vector used in solution
+        _t = np.linspace(0, T, timesteps)
+        # initialize temporary k arrays
+        k1 = np.zeros(U1.shape)
+        k2 = np.zeros(U1.shape)
+        k3 = np.zeros(U1.shape)
+        k4 = np.zeros(U1.shape)
+        n = self.n # index for grabbing position and velocity
+        for i in range(timesteps - 1): # iterate through timesteps
+            dt = _t[i + 1] - _t[i]
+            for j in range(U1.shape[-1]): # dimension
+                k1[:, j] = self.A @ U0[:, j]
+                k2[:, j] = self.A @ (U0[:, j] + 0.5 * dt * k1[:, j])
+                k3[:, j] = self.A @ (U0[:, j] + 0.5 * dt * k2[:, j])
+                k4[:, j] = self.A @ (U0[:, j] + dt * k3[:, j])
+            U1 = U0 + (dt / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
+            if i + 1 in idx_log:
+                self.t[np.where(idx_log == i + 1)] = _t[i + 1]
+                self.X[np.where(idx_log == i + 1)] = U1[:n]
+                self.V[np.where(idx_log == i + 1)] = U1[n:]
+            # reset values
+            U0 = U1
